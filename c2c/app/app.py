296cc6218eb98260c3c863380372325ac2f033a5
Nicholas Ginther <nginth@gmail.com>
2018-03-30 19:13:27 -0500

    Token based auth.

--- c2c/app/app.py
+++ c2c/app/app.py
@@ -5,44 +5,24 @@ from flask_cors import CORS
 
 db = SQLAlchemy()
 
-from app.auth import auth
+from app.auth import init_auth
 
 def create_app(config='config.json'):
     app = Flask(__name__)
-    CORS(app)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://localhost:5432/c2c"
+    CORS(app)    
+    app.config.from_json(config)
+    auth = init_auth(app)
 
     @app.route("/api")
     def api_test():
         return 'hello from the API :)'
-
-    @app.route("/api/register", methods=['POST'])
-    def register():
-        from app.models import User
-        username = request.json.get('username')
-        password = request.json.get('password')
-
-        if username is None or password is None:
-            abort(400, 'Missing username or password.')
-        if User.query.filter_by(username = username).first() is not None:
-            abort(400, 'User already exists.')
-
-        user = User(username = username)
-        user.hash_password(password)
-        db.session.add(user)
-        db.session.commit()
-        return jsonify({ 'username': user.username, 'id': user.id }), 200
-    
-    @app.route("/api/token")
-    @auth.login_required
-    def get_token():
-        token = g.user.generate_auth_token()
-        return jsonify({ 'token': token.decode('ascii') })
-
 
     db.init_app(app)
     from app import models
     with app.app_context():        
         db.create_all()
 
+    from .api.users import users_blueprint
+    app.register_blueprint(users_blueprint, url_prefix='/api/users')
+    
     return app
--- c2c/app/models.py
+++ c2c/app/models.py
@@ -2,15 +2,13 @@ from flask_sqlalchemy import BaseQuery
 # from sqlalchemy_searchable import make_searchable, SearchQueryMixin
 from sqlalchemy_utils.types import TSVectorType
 from passlib.apps import custom_app_context
-from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from app.app import db
-
 
 # make_searchable()
 
 # class SearchQuery(BaseQuery, SearchQueryMixin):
 #     pass
-
 
 class User(db.Model):        
     __tablename__ = 'user'
@@ -27,23 +25,7 @@ class User(db.Model):
         self.password_hash = custom_app_context.encrypt(password)
 
     def verify_password(self, password):
-        return custom_app_context.verify(password, self.password_hash)
-
-    def generate_auth_token(self, expiration = 600):
-        s = Serializer('secret_key', expires_in = expiration)
-        return s.dumps({ 'id': self.id })
-
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer('secret_key')
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            return None # valid token, but expired
-        except BadSignature:
-            return None # invalid token
-        user = User.query.get(data['id'])
-        return user
+        return custom_app_context.verify(password, self.password_hash)    
   
     # search_vector = db.Column(TSVectorType('first_name'))
 
