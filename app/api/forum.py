6522a8d9a9b1cde6931eaa9fb455f450205a48fb
hamzamuhammad <hamzmuha@gmail.com>
2018-04-14 00:46:10 -0500

    refactored react code, adding ajax calls

--- app/api/forum.py
+++ app/api/forum.py
@@ -7,11 +7,11 @@ import time
 
 forum_blueprint = Blueprint('forum', __name__)
 
-# get thread posts for a given page
-@forum_blueprint.route("/forum/<int:page_num>", methods=['GET'])
+# get ALL thread posts
+@forum_blueprint.route("/forum", methods=['GET'])
 @auth.login_required
     def getForum():
-        posts = db.session.query(Post).order_by(-Post.date.desc()).paginate(page_num, 10, False).items
+        posts = db.session.query(Post).order_by(-Post.date.desc()).all()
         threads = [currPost if currPost.thread_id == -1 for currPost in posts]
         return jsonify([post.serialize() for post in threads])
 
@@ -24,8 +24,8 @@ forum_blueprint = Blueprint('forum', __name__)
 
 
 # add post to db (add to parent thread if needed)
-@forum_blueprint.route("/forum", methods=['POST'])
-@forum_blueprint.route("/forum/<int:thread_id>/", methods=['POST'])
+@forum_blueprint.route("/forum/add", methods=['POST'])
+@forum_blueprint.route("/forum/add/<int:thread_id>", methods=['POST'])
 @auth.login_required
     def addPost(thread_id=-1):
 
@@ -43,13 +43,14 @@ forum_blueprint = Blueprint('forum', __name__)
         post.text = text
         post.name = name
         post.date = int(time.time())
+        post.user_id = user_id
         post.thread_id = thread_id
 
         db.session.add(user)
         db.session.commit()
 
 # edit post
-@forum_blueprint.route("/forum/<int:post_id>/", methods=['POST'])
+@forum_blueprint.route("/forum/edit/<int:post_id>", methods=['POST'])
 @auth.login_required
     def editPost():
         if not request.get_json():
@@ -58,18 +59,22 @@ forum_blueprint = Blueprint('forum', __name__)
         data = request.get_json()
         title = data.get('title')
         text = data.get('text')
+        usr_id = data.get('user_id')
 
-        db.session.query().filter_by(id=post_id).update({"title": title, "text": text})
+        db.session.query().filter_by(user_id=usr_id).filter_by(id=post_id).update({"title": title, "text": text})
         db.session.commit()
 
 # delete post + replies
-@forum_blueprint.route("/forum/<int:post_id>/", methods=['POST'])
+@forum_blueprint.route("/forum/delete/<int:post_id>", methods=['POST'])
 @auth.login_required
     def deletePost():
         if not request.get_json():
             abort(400, 'Malformatted JSON request.')
 
-        db.session.query().filter_by(id=post_id).delete()
-        db.session.query().filter_by(thread_id=post_id).delete()
+        data = request.get_json()
+        usr_id = data.get('user_id')
+
+        db.session.query().filter_by(user_id=usr_id).filter_by(id=post_id).delete()
+        db.session.query().filter_by(user_id=usr_id).filter_by(thread_id=post_id).delete()
         db.session.commit()
 
--- src/components/ForumPage.js
+++ src/components/ForumPage.js
@@ -14,127 +14,216 @@ import '../css/LandingPage.css';
 /* Assets */
 import BlueSymbol from '../assets/C2C_symbol.jpg';
 
-class Post extends Component {
+/* 
+there are 4 types of posts a forum can have:
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: null,
-      text: null,
-      date: null,
-      name: null,
-    };
-  }
+  1 - a Thread posted by someone else
+  2 - a Thread posted by current user
+  3 - a Reply posted by someone else
+  4 - a Reply posted by current user
 
-  render() {  
-    return (
-      <div class="card mb-4" class="w-75">
-          <div class="card-body">
-            <h3 class="card-title">this.props.title</h3>
-            <p class="card-text">this.props.text</p>
-            <button type="button" class="btn btn-info" onClick={() => this.props.onView()}>View</button>
-          </div>
-          <div class="card-footer text-muted">
-            Posted on this.props.date by
-            <a href="#"> this.props.name</a>
-            <button type="button" class="btn btn-primary" onClick={() => this.props.onReply()}>Reply</button>
-          </div>
+these distinctions are mainly used for what actions
+can be performed on each element as well as which
+buttons to display
+
+then, a parent component ForumPage handles all
+the logic
+
+*/
+
+/* */
+function SelfThreadPost(props) {
+  return (
+    <div class="card mb-4" class="w-75">
+      <div class="card-body">
+        <h3 class="card-title">props.title</h3>
+        <p class="card-text">props.text</p>
+        <button type="button" class="btn btn-info" onClick={props.onView}>View</button>
+      </div>
+      <div class="card-footer text-muted">
+        Posted on props.date by
+        <a href="#"> props.name</a>
+        <button type="button" class="btn btn-primary" onClick={props.onReply}>Reply</button>
+        <button type="button" class="btn btn-primary" onClick={props.onEdit}>Edit</button>
+        <button type="button" class="btn btn-primary" onClick={props.onDelete}>Delete</button>
       </div>
       <br />
-    );
-  }
+    </div>
+  );
 }
 
-/* Takes care of presenting posts/replies + pagination */
-/* TODO: have a global data structure to contain posts/replies */
+/* */
+function SelfReplyPost(props) {
+  return (
+    <div class="card mb-4" class="w-60">
+      <div class="card-body">
+        <h3 class="card-title">props.title</h3>
+        <p class="card-text">props.text</p>
+      </div>
+      <div class="card-footer text-muted">
+        Posted on props.date by
+        <a href="#"> props.name</a>
+        <button type="button" class="btn btn-primary" onClick={props.onEdit}>Edit</button>
+        <button type="button" class="btn btn-primary" onClick={props.onDelete}>Delete</button>
+      </div>
+      <br />
+    </div>
+  );
+}
+
+/* */
+function PublicThreadPost(props) {
+  return (
+    <div class="card mb-4" class="w-75">
+      <div class="card-body">
+        <h3 class="card-title">props.title</h3>
+        <p class="card-text">props.text</p>
+        <button type="button" class="btn btn-info" onClick={props.onView}>View</button>
+      </div>
+      <div class="card-footer text-muted">
+        Posted on props.date by
+        <a href="#"> props.name</a>
+        <button type="button" class="btn btn-primary" onClick={props.onReply}>Reply</button>
+      </div>
+      <br />
+    </div>
+  );
+}
+
+/* */
+function PublicReplyPost(props) {
+  return (
+    <div class="card mb-4" class="w-60">
+      <div class="card-body">
+        <h3 class="card-title">props.title</h3>
+        <p class="card-text">props.text</p>
+      </div>
+      <div class="card-footer text-muted">
+        Posted on props.date by
+        <a href="#"> props.name</a>
+      </div>
+      <br />
+    </div>
+  );
+}
+
+/* manages entire forum */
 class ForumPage extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       posts: null,
-      index: 1,
-    };
+    }
   }
 
-  /* adds a reply to the thread */
-  /* TODO: with edits, check if userid matches */
-  handleReply(id) {
-
+  /* when user decides to make a new thread */
+  handleNewThread(data) {
+    let callback = this.getPosts;
+    fetch('https://code-2-college-connect-api.herokuapp.com/api/forum/add', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    }).then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      }
+      // Handle bad password or username
+      else if (resp.status != 200) {
+        console.log("Error making call status: " + resp.status);
+      }
+    }).catch(function(e) {
+      console.error("Error: " + e);
+    });
   }
 
-  /* called to display more replies for a given post update */
+  /* make ajax call to pull replies for given thread */
   handleView(id) {
-
+    /* pull all posts with thread_id==id*/
   }
 
-  /* modify db entry as well as update view */
+  /* add reply to DB and re-render posts */
+  handleReply(id) {
+    /* add post to db and make thread_id=id*/
+  }
+
+  /* edit value in DB and re-render posts */
   handleEdit(id) {
 
   }
 
-  /* delete post + remove from view */
+  /* remove value in DB and re-render posts */
   handleDelete(id) {
 
   }
 
-  renderPost(id) {
+  renderSelfThreadPost(postData) {
     return (
-      <Post
-        title={this.posts[id].title}
-        text={this.posts[id].text}
-        date={this.posts[id].date}
-        name={this.posts[id].name}
+      <SelfThreadPost
+        title={postData.title}
+        text={postData.text}
+        date={postData.date}
+        name={postData.name}
 
-        onView={() => this.handleView(id)}
-        onReply={() => this.handleReply(id)}
-        onEdit={() => this.handleEdit(id)}
-        onDelete={() => this.handleDelete(id)}
+        onView={() => this.handleView(postData.id)}
+        onReply={() => this.handleReply(postData.id)}
+        onEdit={() => this.handleEdit(postData.id)}
+        onDelete={() => this.handleDelete(postData.id)}
       />
     );
   }
 
-  decreasePage() {
-    this.props.index -= 1
-    getPosts()
-  }
-
-  increasePage() {
-    this.props.index += 1
-    getPosts()
-  }
-
-  renderFooter() {
+  renderSelfReplyPost(postData) {
     return (
-      <div>
-        <ul class="pagination justify-content-center mb-4">
-          <li class="page-item">
-            <button type="button" class="btn btn-info" onClick={() => this.props.decreasePage()}>&larr; Older</button>
-          </li>
-          <li class="page-item disabled">
-            <button type="button" class="btn btn-info" onClick={() => this.props.increasePage()}>Newer &rarr;</button>
-          </li>
-        </ul>
-      </div>
+      <SelfReplyPost
+        title={postData.title}
+        text={postData.text}
+        date={postData.date}
+        name={postData.name}
+
+        onEdit={() => this.handleEdit(postData.id)}
+        onDelete={() => this.handleDelete(postData.id)}
+      />
+    );    
+  }
+
+  renderPublicThreadPost(postData) {
+    return (
+      <PublicThreadPost
+        title={postData.title}
+        text={postData.text}
+        date={postData.date}
+        name={postData.name}
+
+        onView={() => this.handleView(postData.id)}
+        onReply={() => this.handleReply(postData.id)}
+      />
     );
   }
 
-  componentDidMount() {
-    this.getPosts();
+  renderPublicReplyPost(postData) {
+    return (
+      <PublicReplyPost
+        title={postData.title}
+        text={postData.text}
+        date={postData.date}
+        name={postData.name}
+      />
+    );      
   }
 
-  /*make dict with postid->post attributes*/
-  setPosts(result) {
+  setPosts(data) {
     this.setState({
-      posts: result
+      posts: JSON.parse(data)
     });
   }
 
   getPosts() {
-
     let callback = this.setPosts;
-
-    fetch('https://code-2-college-connect-api.herokuapp.com/api/forum/' + this.props.index, {
+    fetch('https://code-2-college-connect-api.herokuapp.com/api/forum/', {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
@@ -159,14 +248,39 @@ class ForumPage extends Component {
         });
   }
 
+  /* initial pull of all posts*/
+  componentDidMount() {
+    this.getPosts();
+  }
+
   render() {
-    let postItems = [];
-    let keys = Object.keys(this.posts)
-    for (currKey in keys) {
-      postItems.push(renderPost(currKey))
+    if (this.posts == null) {
+      return <div>Loading...</div>;
     }
-    postItems.push(renderFooter())
-    return <div>{postItems}</div>;
+
+    /* iterate thru json + generate div objects */
+    let items = [];
+    for (post in this.posts) {
+      let post_id = post;
+      let title = this.posts[post]["title"];
+      let text = this.posts[post]["title"];
+      let date = this.posts[post]["title"];
+      let name = this.posts[post]["title"];
+      let user_id = this.posts[post]["user_id"];
+      let thread_id = this.posts[post]["thread_id"];
+
+      let data = {"title":title,"text":text,"date":date,"name":name,"id":post_id};
+
+      /* guarenteed to be THREAD objects, so check if editable or not*/
+      if this.props.id == user_id {
+        items.push(this.renderSelfThreadPost(data));
+      }
+      else {
+        items.push(this.renderPublicThreadPost(data));
+      }
+    }
+
+    return <div>{items}</div>;
   }
 }
 
